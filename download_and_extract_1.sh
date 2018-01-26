4e665897990f9c20f901de4b2f4c61ec3e987b41
Thomas Elder <tgelder@gmail.com>
2018-01-26 19:43:04 +0000

    Renaming bird_extractor to image_extractor
    Separate script for book 1 and 2

--- download_and_extract_1.sh
+++ download_and_extract_1.sh
@@ -1,5 +1,5 @@
 page_to=`expr $1 + 1`
 python3 download_images.py "https://ia802708.us.archive.org/BookReader/BookReaderImages.php?zip=/5/items/memorialeditiono01bewirich/memorialeditiono01bewirich_jp2.zip&file=memorialeditiono01bewirich_jp2/memorialeditiono01bewirich_{}.jp2&scale=1&rotate=0" "." --from_page=$1 --to_page=$page_to
 printf -v p "%04d" $1
-python3 bird_extractor.py $p.jpg $2.png
+python3 image_extractor.py $p.jpg $2.png
 xdg-open $2.png
