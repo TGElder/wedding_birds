bfe83ebd2497d19c12fb18241850489dc51301ce
Thomas Elder <tgelder@gmail.com>
2018-01-27 19:52:01 +0000

    Changing image extractor to take a directory + other enhancements

--- book_2.sh
+++ book_2.sh
@@ -1,1 +1,1 @@
-python3 download_images.py "https://ia801406.us.archive.org/BookReader/BookReaderImages.php?zip=/8/items/memorialeditiono02bewirich/memorialeditiono02bewirich_jp2.zip&file=memorialeditiono02bewirich_jp2/memorialeditiono02bewirich_{}.jp2&scale=1&rotate=0" "book_1" --to_page=456
+python3 download_images.py "https://ia801406.us.archive.org/BookReader/BookReaderImages.php?zip=/8/items/memorialeditiono02bewirich/memorialeditiono02bewirich_jp2.zip&file=memorialeditiono02bewirich_jp2/memorialeditiono02bewirich_{}.jp2&scale=1&rotate=0" "book_2" --to_page=456
--- image_extractor.py
+++ image_extractor.py
@@ -11,34 +11,44 @@ from skimage.measure import label, regionprops
 from skimage.morphology import closing, square
 from skimage.color import rgb2gray, label2rgb
 
-parser = argparse.ArgumentParser(description="Extracts largest object from page")
+parser = argparse.ArgumentParser(description="Extracts pictures from scanned page")
 parser.add_argument("input")
-parser.add_argument("output")
+parser.add_argument("output_directory")
+parser.add_argument("--minimum_object_size_pc", type=float, default=0.005)
+parser.add_argument("--threshold", type=float, default=0.5)
 args = parser.parse_args()
 
-# Loading image
-print("Loading image")
-image = io.imread(args.input)
-image = rgb2gray(image)
+if os.path.isdir(args.input):
+    files = sorted([os.path.join(args.input, file) for file in os.listdir(args.input)])
+else:
+    files = [args.input]
+    
+for file in files:
+    print("Loading image from {}".format(file))
+    image = io.imread(file)
+    image = rgb2gray(image)
+    image_area = image.size
 
-print("Finding largest object")
-# apply threshold
-thresh = threshold_otsu(image)
-bw = closing(image < thresh, square(10))
+    print("Finding objects")
+    thresh = threshold_otsu(image)
+    bw = closing(image < thresh, square(10))
 
-# remove artifacts connected to image border
-cleared = clear_border(bw)
+    cleared = clear_border(bw)
 
-# label image regions
-label_image = label(cleared)
-image_label_overlay = label2rgb(label_image, image=image)
+    label_image = label(cleared)
+    image_label_overlay = label2rgb(label_image, image=image)
 
-sorted_regions = sorted(regionprops(label_image), key=lambda region: region.area, reverse=True)
-largest_region = sorted_regions[0]
-
-print("Cropping and saving")
-minr, minc, maxr, maxc = largest_region.bbox
-cropped = image[minr:maxr, minc:maxc]
-thresh = threshold_otsu(image)
-thresholded = (cropped > 0.5) * 255
-io.imsave(args.output, thresholded)
+    output_regions = [region for region in regionprops(label_image) if region.area/image_area >= args.minimum_object_size_pc]
+    print("Found {} objects to output".format(len(output_regions)))
+    output_count = 0
+    for output_region in output_regions:
+        output_count += 1
+        print("Cropping and saving object {}".format(output_count))
+        minr, minc, maxr, maxc = output_region.bbox
+        cropped = image[minr:maxr, minc:maxc]
+        thresholded = (cropped > args.threshold) * 255
+        file_name, file_extension = os.path.splitext(os.path.basename(file))
+        output_file = "{}-{}.png".format(file_name, str(output_count))
+        if not os.path.exists(args.output_directory):
+            os.makedirs(args.output_directory)
+        io.imsave(os.path.join(args.output_directory, output_file), thresholded)
