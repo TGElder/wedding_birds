972a921504cea527e9401d25c5376f6c75b3ed98
Thomas Elder <tgelder@gmail.com>
2018-02-03 16:10:02 +0000

    Fully working create_place_name.py

--- create_place_name.py
+++ create_place_name.py
@@ -2,16 +2,10 @@ import os
 import argparse
 import numpy
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-
 from skimage import data, io
-from skimage.filters import threshold_otsu
-from skimage.segmentation import clear_border
-from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
-from skimage.color import rgb2gray, label2rgb
 from skimage import img_as_ubyte
+
+import text_renderer
 
 parser = argparse.ArgumentParser(description="Extracts pictures from scanned page")
 parser.add_argument("input")
@@ -19,48 +13,58 @@ parser.add_argument("output_directory")
 parser.add_argument("--output_ratio", type=float, default=2.8286)
 parser.add_argument("--x_margin", type=float, default=0.05)
 parser.add_argument("--y_margin", type=float, default=0.05)
-parser.add_argument("--text_width", type=float, default=0.5)
+parser.add_argument("--text_area_width", type=float, default=0.5)
 args = parser.parse_args()
 
 output_ratio = args.output_ratio
 x_margin = args.x_margin
 y_margin = args.y_margin
-text_width = args.text_width
+text_area_width = args.text_area_width
 
 if os.path.isdir(args.input):
     files = sorted([os.path.join(args.input, file) for file in os.listdir(args.input)])
 else:
     files = [args.input]
     
-bird_area_width = 1 - x_margin * 2 - text_width
+bird_area_width = 1 - x_margin * 2 - text_area_width
 bird_area_height = (1 - y_margin * 2) / output_ratio
 bird_area_ratio = bird_area_width / bird_area_height 
-print("Bird area dimensions={}x{}={}".format(bird_area_width, bird_area_height, bird_area_ratio))
+print("bird_area_ratio={}".format(bird_area_ratio))
 
 for file in files:
 
     file_name, file_extension = os.path.splitext(os.path.basename(file))
-    print("Generating place name for {}", file_name)
+    if "!" in file_name:
+        flip = True
+        file_name = file_name.replace("!", "")
+    else:
+        flip = False
+    print("Generating place name for {}".format(file_name))
 
     print("Loading image from {}".format(file))
     image = io.imread(file)
     height, width = image.shape
-    ratio = width / height
+    if flip:
+        print("Flipping image")
+        image = numpy.fliplr(image)
 
-    print("dimensions={}x{}".format(width, height))
-    print("ratio={}".format(ratio))
-    print("bird_area_ratio={}".format(bird_area_ratio))
+    print("Image dimensions = {}x{}".format(width, height))
+    ratio = (width*1.0) / (height*1.0)
+    print(ratio)
+
     if ratio >= bird_area_ratio:
         bird_area_width = width
+        bird_area_height = width / bird_area_ratio
     else:
         bird_area_width = height * bird_area_ratio
+        bird_area_height = height
 
-    print("bird_area_width={}".format(bird_area_width))
-    output_width = bird_area_width / (1 - text_width - x_margin * 2)
+    print("bird_area_dimensions={}x{}".format(bird_area_width, bird_area_height))
+    output_width = bird_area_width / (1 - text_area_width - x_margin * 2)
     output_width = int(output_width)
     output_height = output_width / output_ratio  
     output_height = int(output_height)
-    output_text_width = output_width * text_width
+    output_text_width = output_width * text_area_width
     output_text_width = int(output_text_width)
     print("output_dimensions={}x{}".format(output_width, output_height))
     margin_width = output_width * x_margin
@@ -81,6 +85,15 @@ for file in files:
     print("{}:{} {}:{}".format(minr, maxr, minc, maxc))
 
     output_image[minr:maxr, minc:maxc] = image
+
+    text_desired_height = int(bird_area_height / 4)
+    print("Desired text height = {}".format(text_desired_height))
+    text_image = text_renderer.get_text_image(file_name, text_desired_height)
+    (text_height, text_width) = text_image.shape
+    text_ystart = (output_height - text_desired_height) / 2
+    text_xstart = (output_width - width - margin_width - text_width) / 2
+
+    output_image[text_ystart:text_ystart + text_height, text_xstart:text_xstart + text_width] = text_image
 
     output_file = "{}.png".format(file_name)
     if not os.path.exists(args.output_directory):
---
+++ text_renderer.py
@@ -0,0 +1,47 @@
+import cairo
+import pango
+import pangocairo
+import sys
+import numpy as np
+
+def render_text(text, height, out_file):
+    surf = cairo.ImageSurface(cairo.FORMAT_A1, height * 8, height * 2)
+    context = cairo.Context(surf)
+
+    context.rectangle(0,0,height * 8,height * 2)
+
+    context.set_antialias(cairo.ANTIALIAS_SUBPIXEL)
+
+    pangocairo_context = pangocairo.CairoContext(context)
+    pangocairo_context.set_antialias(cairo.ANTIALIAS_SUBPIXEL)
+
+    layout = pangocairo_context.create_layout()
+    font = pango.FontDescription("Book Antiqua")
+    font.set_size(1024 * height)
+    layout.set_font_description(font)
+
+    layout.set_text(text)
+    pangocairo_context.update_layout(layout)
+    pangocairo_context.show_layout(layout)
+
+    with open(out_file, "wb") as image_file:
+        surf.write_to_png(image_file)
+
+def bounding_box(image):
+    (rows, columns) = image.nonzero()
+    return (min(rows), max(rows), min(columns), max(columns))
+
+def get_text_image(text, height):
+    render_text(text, height, "text_temp.png")
+
+    from skimage import data, io
+        
+    image = io.imread("text_temp.png")
+
+    (minr, maxr, minc, maxc) = bounding_box(image)
+
+    #inverting
+    image = 255 - image
+    cropped = image[minr:maxr, minc:maxc]
+
+    return cropped
